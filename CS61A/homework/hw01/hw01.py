1306413e3d8c8a69c1f32d4359f55a75ad88961c
RyanFcr <3200103626@zju.edu.cn>
2022-07-23 12:04:09 +0800

    HW1

--- CS61A/homework/hw01/hw01.py
+++ CS61A/homework/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     ['return f(a, b)']
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 
@@ -37,7 +37,7 @@ def two_of_three(x, y, z):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
     ['Expr', 'Return']
     """
-    return _____
+    return x*x+y*y+z*z-max(x,y,z)*max(x,y,z)
 
 
 def largest_factor(n):
@@ -51,6 +51,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    x = n - 1
+    while x > 0:
+        if(n % x==0):
+            return x
+        x = x - 1
 
 
 def if_function(condition, true_result, false_result):
@@ -96,12 +101,15 @@ def with_if_function():
 
 def cond():
     "*** YOUR CODE HERE ***"
+    return 0 and true_func()
 
 def true_func():
     "*** YOUR CODE HERE ***"
+    print("42")
 
 def false_func():
     "*** YOUR CODE HERE ***"
+    print("47")
 
 
 def hailstone(n):
@@ -120,4 +128,17 @@ def hailstone(n):
     7
     """
     "*** YOUR CODE HERE ***"
+    count = 1
+    print(n)
+    while(n!=1):
+        if(n%2==0):
+            n=n//2
+            print(n)
+            count = count +1
+        else:
+            n=3*n+1
+            print(n)
+            count = count +1
+    return count
+
 
