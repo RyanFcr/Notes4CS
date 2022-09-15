6efd104512269909c70187fc90012077842a0785
RyanFcr <3200103626@zju.edu.cn>
2022-09-15 23:59:16 +0800

    HW6

---
+++ CS61A/homework/hw06/hw06.scm
@@ -0,0 +1,23 @@
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  (cdr s)
+)
+
+(define (caddr s)
+  
+)
+
+
+(define (sign num)
+  'YOUR-CODE-HERE
+)
+
+
+(define (square x) (* x x))
+
+(define (pow x y)
+  'YOUR-CODE-HERE
+)
+
